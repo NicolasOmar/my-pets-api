33518bd83de8cf367adf80ce523692b713ffa196
Nicolás Omar González Passerino <NicolasOmar>
2021-05-29 23:16:34 -0300

    Fixing module types error in ci

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,8 +1,6 @@
-import util from 'util';
-import child_process from 'child_process'
-import prompts from 'prompts';
-
-const exec = util.promisify(child_process.exec);
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const prompts = require('prompts');
 
 const runVersionUpdate = async () => {
   try {
