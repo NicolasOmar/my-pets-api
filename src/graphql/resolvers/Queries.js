0a14d349c1fe709653d615c96c11f155dfe3efe0
NicolasOmar <nicolas.passerino@gmail.com>
2022-03-19 13:04:33 -0300

    PetTypes added in mongoose and graphql. Query implemented

--- src/graphql/resolvers/Queries.js
+++ src/graphql/resolvers/Queries.js
@@ -1,3 +1,5 @@
+import PetType from '../../db/models/petTypes.model'
+
 const Queries = {
   getUser: async (_, __, { loggedUser, token }) => {
     return {
@@ -6,7 +8,8 @@ const Queries = {
       email: loggedUser.email,
       token
     }
-  }
+  },
+  getPetTypes: async () => (await PetType.find()).map(({ _id, type }) => ({ id: _id, name: type }))
 }
 
 export default Queries
