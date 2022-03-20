10ea8ca289ae67deaa3017c12d0302576aaf3a4d
NicolasOmar <nicolas.passerino@gmail.com>
2022-03-20 01:45:21 -0300

    Database generator added for new secondary data

--- src/functions/populate.js
+++ src/functions/populate.js
@@ -3,12 +3,15 @@ import PetType from '../db/models/petTypes.model'
 // MOCKS
 import { petTypesStart } from './mocks/populate.mocks.json'
 
-const logger = (entity, data) => console.log(`${entity} created => ${data}`)
+export const logger = (entity, data) => console.log(`${entity} created => ${data}`)
 
-export const populatePetTypes = async () => {
-  petTypesStart.forEach(async _petType => {
-    const mongoPetType = new PetType({ ..._petType })
-    const createdPetType = await mongoPetType.save()
-    logger('[Pet Type]', createdPetType)
-  })
+export const populatePetTypes = () => {
+  return Promise.allSettled(
+    petTypesStart.map(_petType => {
+      return new Promise(resolve => {
+        const mongoPetType = new PetType({ ..._petType })
+        resolve(mongoPetType.save())
+      })
+    })
+  )
 }
