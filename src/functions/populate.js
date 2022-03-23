2332c363a600c05cadce7f77d8fc1675747cf7f2
NicolasOmar <nicolas.passerino@gmail.com>
2022-03-23 01:49:55 -0300

    Small changes on population method for table deletion. WIP

--- src/functions/populate.js
+++ src/functions/populate.js
@@ -1,17 +1,21 @@
 // DB
 import PetType from '../db/models/petTypes.model'
 // MOCKS
-import { petTypesStart } from './mocks/populate.mocks.json'
+import { petTypeSeeds } from './mocks/populate.mocks.json'
 
 export const logger = (entity, data) => console.log(`${entity} created => ${data}`)
 
 export const populatePetTypes = () => {
-  return Promise.allSettled(
-    petTypesStart.map(_petType => {
+  return Promise.allSettled([
+    ...petTypeSeeds.map(name => {
       return new Promise(resolve => {
-        const mongoPetType = new PetType({ ..._petType })
+        const mongoPetType = new PetType({ name })
         resolve(mongoPetType.save())
       })
     })
-  )
+  ])
+}
+
+export const dropTables = () => {
+  return Promise.allSettled([new Promise(resolve => resolve(PetType.deleteMany()))])
 }
