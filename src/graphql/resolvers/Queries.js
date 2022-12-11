da666d007b21309b5deb0823b254459a35bda289
NicolasOmar <nicolas.passerino@gmail.com>
2022-12-11 05:10:44 -0300

    Adding first approach to handle Widget data

--- src/graphql/resolvers/Queries.js
+++ src/graphql/resolvers/Queries.js
@@ -5,6 +5,7 @@ import Pet from '../../db/models/pet.model'
 import User from '../../db/models/user.model'
 import { ApolloError } from 'apollo-server-errors'
 import { ERROR_MSGS, HTTP_CODES } from '../../constants/errors.json'
+import { findIds } from '../../functions/parsers'
 
 const Queries = {
   getUser: async (_, __, { loggedUser, token }) => ({
@@ -35,6 +36,24 @@ const Queries = {
     }
 
     return foundedPet
+  },
+  getMyPetsQuantity: async (_, __, { loggedUser }) => {
+    if (!loggedUser) {
+      throw new ApolloError(ERROR_MSGS.MISSING_USER_DATA, HTTP_CODES.UNAUTHORIZED)
+    }
+
+    const { _id } = await User.findOne({ userName: loggedUser.userName })
+    const petPopulation = await Pet.find({ user: _id })
+    const populationInfo = Promise.allSettled(
+      petPopulation.map(pet => new Promise(resolve => resolve(findIds(PetType, pet.petType))))
+    )
+    const explicitInfo = (await populationInfo).map(data => data.value[0].name)
+    const finalInfo = Array.from(new Set(explicitInfo)).map(info => ({
+      name: info,
+      quantity: explicitInfo.filter(_info => _info === info).length
+    }))
+
+    return [{ name: 'All', quantity: petPopulation.length }, ...finalInfo]
   }
 }
 
