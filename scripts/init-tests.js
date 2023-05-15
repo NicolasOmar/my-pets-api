20c3ea9b8f2deee54ca7635ac6923b988ecade0c
NicolasOmar <nicolas.passerino@gmail.com>
2023-05-15 00:39:17 -0300

    Updating every dependency. Node and Jest working as before

--- scripts/init-tests.js
+++ scripts/init-tests.js
@@ -1,11 +1,11 @@
-require('@babel/register')
+import '@babel/register'
 // APP AND SERVER IMPORTS
 const server = require('../src/server/server').default
 const app = require('../src/server/app').default
 // ENVIRONMENTS VARIABLES
 const { PORT } = process.env
 
-module.exports = async () => {
+export default async () => {
   await server.start()
   server.applyMiddleware({ app })
   app.listen(PORT)
